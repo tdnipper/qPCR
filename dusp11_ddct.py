c3ffeda7ba5ec4b14cf9dc2d1d2e745d12447755
Thomas Nipper <nipper@wisc.edu>
2023-03-28 14:17:12 -0500

    Make further functions

--- dusp11_ddct.py
+++ dusp11_ddct.py
@@ -33,9 +33,9 @@ rep3 = import_file(sys.argv[3])
 def drop_row(df: pd.DataFrame, x) -> pd.DataFrame:
     """Drop a row if it contains a string"""
     str_drop = str(x)
-    mask =df['Sample Name'].str.contains(str_drop)
+    mask = df['Sample Name'].str.contains(str_drop)
     df = df.drop(df[mask].index)
-    return(df)
+    return (df)
 
 rep1 = drop_row(rep1, 'DUSP11KO')
 rep2 = drop_row(rep2, 'DUSP11KO')
@@ -43,4 +43,18 @@ rep3 = drop_row(rep3, 'DUSP11KO')
 
 frames = [rep1, rep2, rep3]
 combo = pd.concat(frames)
-print(combo)
+
+# Get T16 and T24 separated into 18s and DUSP11
+
+t16_18s_subset = combo.loc[combo['Sample Name'].str.contains('WT T16') &
+                    combo['Target Name'].str.contains('18S')
+]
+t16_DUSP11_subset = combo.loc[combo['Sample Name'].str.contains('WT T16') &
+                    combo['Target Name'].str.contains('DUSP11')
+]
+t16_18s_subset.index = range(len(t16_18s_subset))
+t16_DUSP11_subset.index = range(len(t16_DUSP11_subset))
+# t16_18s_subset= t16_18s_subset[~t16_18s_subset.index.duplicated(keep='first')]
+t16 = pd.concat([t16_18s_subset, t16_DUSP11_subset], ignore_index=True, axis=1)
+t16.groupby(['0'].str.get(0))
+print(t16)
