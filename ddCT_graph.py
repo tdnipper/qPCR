fc2096f9d1381e53a51ae736ad674e54988a9df2
Thomas Nipper <nipper@wisc.edu>
2023-04-07 14:22:16 -0500

    Added graphing program file

---
+++ ddCT_graph.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser(description="Take ddCT file and graph means and std")
+# Define and use input argments for file, mean, std columns. Set defaults for columns if no input
+parser.add_argument("-i", "--input", type=str, help="Name of input file")
+parser.add_argument("-y", "--yaxis", type=str, help="Name of column with y values")
+parser.add_argument("-s", "--stdev", type=str, help="Name of column with stdev")
+parser.add_argument("-n", "--samplename", type=str, help="Name of Sample Name column")
+parser.add_argument("-o", "--outfile", type=str, help="Destination file to save")
+
+args = parser.parse_args()
+
+if args.input:
+    infile = args.input
+
+if args.yaxis:
+    mean = args.yaxis
+else:
+    mean = "foldchange"
+
+if args.stdev:
+    std = args.stdev
+else:
+    std = "std"
+
+if args.samplename:
+    name = args.samplename
+else:
+    name = "Sample Name"
+
+if args.outfile:
+    outfile = args.outfile
+else:
+    outfile = "ddCT.png"
+
+infile = pd.read_excel(infile)
+
+sns.set(style="darkgrid")
+plot = sns.barplot(data=infile, x=name, y=mean, errorbar="sd")
+plt.savefig(outfile, dpi="300")
+plt.show(plot)
