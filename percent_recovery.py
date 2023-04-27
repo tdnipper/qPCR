6756b20047a570586d90aa98a73114431f78cb8e
Thomas Nipper <nipper@wisc.edu>
2023-04-27 10:50:27 -0500

    Moved files, got input and enrich working, output to table, update JSON

--- percent_recovery.py
+++ percent_recovery.py
@@ -1,5 +1,6 @@
 import argparse
 import pandas as pd
+import numpy as np
 
 parser = argparse.ArgumentParser(
     description="Analyse % recovery from cappable seq files"
@@ -14,11 +15,20 @@ parser.add_argument(
     help="Key file with names of transcripts to be analyzed in a column",
 )
 
-parser.add_argument("-e", "--enrich", type=str, help="File with enriched samples")
+parser.add_argument(
+    "-e", "--enrich", type=str, nargs="+", help="File with enriched samples"
+)
+
+parser.add_argument(
+    "-o", "--outfile", type=str, help="Name of outfile for graph and table results"
+)
 
 args = parser.parse_args()
+
 infile = args.input
 efile = args.enrich
+outfile = args.outfile
+
 if args.key:
     key = args.key
 
@@ -34,10 +44,18 @@ if args.key:
 
 # Import files for subsequent analysis
 def import_file(filename) -> pd.DataFrame:
-    df = pd.read_excel(filename)
+    """This function will import and format replicate files."""
+    df = pd.read_excel(
+        filename,
+        sheet_name="Results",
+        skiprows=46,
+        usecols=["Sample Name", "Target Name", "CT"],
+        na_values="Undetermined",
+    )
+    return df
 
 
-# Use a loop to iterate import_file over all filenames in
+# Import input and enrich files and append any replicate files
 files_input = []
 for file in infile:
     file_in = import_file(file)
@@ -47,7 +65,19 @@ for file in efile:
     file_in = import_file(file)
     files_enrich.append(file_in)
 
+# Concat input and enrich into dataframes, then subtract enrich from input
 concat_input = pd.concat(files_input, ignore_index=True)
 concat_enrich = pd.concat(files_enrich, ignore_index=True)
-
-print(concat_enrich)
+ct_recover = pd.DataFrame(
+    {
+        "Sample Name": concat_input["Sample Name"],
+        "Target Name": concat_input["Target Name"],
+        "CT_input": concat_input["CT"],
+        "CT_enrich": concat_enrich["CT"],
+        "CT_diff": concat_input["CT"].sub(concat_enrich["CT"]),
+    }
+)
+ct_recover["percent_recovery"] = 100 * 2 ** ct_recover["CT_diff"]
+print(ct_recover)
+# Save final table to excel
+ct_recover.to_excel(f'{outfile}.xlsx')
