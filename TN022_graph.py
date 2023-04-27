e7049e29ff3218f10ac05824f6ab63fe1e6d03f2
Thomas Nipper <nipper@wisc.edu>
2023-04-27 13:03:14 -0500

    Graphing in 4 grid

--- TN022_graph.py
+++ TN022_graph.py
@@ -3,13 +3,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_excel("TN022.1_output.xlsx")
-plot = sns.FacetGrid(data, col="Sample Name")
-plot.map(
-    sns.histplot,
+
+# # Create barplot using facet grid separated by Sample Name
+# plot = sns.FacetGrid(data,
+#                      col="Sample Name",
+#                      height=4,
+#                      aspect=2)
+# plot.map(sns.barplot,
+#          data=data,
+#          x="Target Name",
+#          y="percent_recovery")
+# plot.set_xticklabels(rotation=45)
+# plt.subplots_adjust(bottom=0.3)
+# plt.show()
+
+# Create catplot separating by Sample Name and showing each target
+g = sns.catplot(
     data=data,
+    kind="bar",
     x="Target Name",
     y="percent_recovery",
+    hue="Sample Name",
+    col="Sample Name",
+    col_wrap=2,
+    height=4,
+    aspect=1.2,
+    palette="muted",
 )
-plot.set_xticklabels(rotation=45)
-plt.subplots_adjust(bottom=0.3)
-plt.show()
+
+# Rotate x-axis labels for all plots
+g.set_xticklabels(rotation=45, horizontalalignment="right")
+
+# Show the plot
+sns.despine()
+sns.set_style("whitegrid")
+# plt.show()
+plt.savefig("TN022.1_sep_output.png", format="png", dpi=300)
