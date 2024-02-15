ee493e8b5f0e6b07f038736e593f222bd037853b
Thomas Nipper <nipper@wisc.edu>
2024-02-15 14:48:25 -0600

    Change x-axis to temperature

--- TN031/TN031.9.py
+++ TN031/TN031.9.py
@@ -13,8 +13,7 @@ def import_file(filename):
             skiprows=46,
             usecols=["Sample Name", "Target Name", "CT"],
             na_values="Undetermined",
-        )
-        .dropna()
+        ).dropna()
         # .groupby(by=["Sample Name", "Target Name"])
         # .mean()
         # .reset_index()
@@ -63,8 +62,8 @@ dCT_mock["mock 48"] = mock_dusp11_48["CT"].sub(mock_actin_48["CT"])
 dCT_dox["dox 48"] = dox_dusp11_48["CT"].sub(dox_actin_48["CT"])
 
 ddCT = pd.DataFrame()
-ddCT['24 ddCT'] = dCT_dox['dox 24'].sub(dCT_mock['mock 24'])
-ddCT['48 ddCT'] = dCT_dox['dox 48'].sub(dCT_mock['mock 48'])
+ddCT["24 ddCT"] = dCT_dox["dox 24"].sub(dCT_mock["mock 24"])
+ddCT["48 ddCT"] = dCT_dox["dox 48"].sub(dCT_mock["mock 48"])
 
 ddCT_foldchange = pd.DataFrame()
 ddCT_foldchange["24"] = np.power(2, -(ddCT["24 ddCT"]))
@@ -73,6 +72,7 @@ ddCT_foldchange["48"] = np.power(2, -(ddCT["48 ddCT"]))
 # Save final table to an excel file
 output = pd.concat([ddCT, ddCT_foldchange], axis=1)
 output.to_excel("TN031.9 ddCT.xlsx")
+
 
 # Plot foldchange
 def plot_foldchange(df, title):
@@ -85,38 +85,43 @@ def plot_foldchange(df, title):
     ax.set_ylabel("Foldchange")
     plt.savefig(f"TN031.9 {title}.png", dpi=300)
 
+
 plot_foldchange(ddCT_foldchange, "DUSP11 Foldchange during CRISPRi induction")
 
 # Import melt-curve data
 
-melt_curve = pd.read_excel("TN031_9.xlsx", sheet_name="Melt Curve Raw Data", skiprows=46)
+melt_curve = pd.read_excel(
+    "TN031_9.xlsx", sheet_name="Melt Curve Raw Data", skiprows=46
+)
 # Make dictionary to rename columns
 rename = {
-    "A1":"mock 24 DUSP11",
-    "A2":"mock 24 DUSP11",
-    "A3":"mock 24 DUSP11",
-    "A4":"mock 24 actin",
-    "A5":"mock 24 actin",
-    "A6":"mock 24 actin",
-    "B1":"mock 48 DUSP11",
-    "B2":"mock 48 DUSP11",
-    "B3":"mock 48 DUSP11",
-    "B4":"mock 48 actin",
-    "B5":"mock 48 actin",
-    "B6":"mock 48 actin",
-    "C1":"dox 24 DUSP11",
-    "C2":"dox 24 DUSP11",
-    "C3":"dox 24 DUSP11",
-    "C4":"dox 24 actin",
-    "C5":"dox 24 actin",
-    "C6":"dox 24 actin",
-    "D1":"dox 48 DUSP11",
-    "D2":"dox 48 DUSP11",
-    "D3":"dox 48 DUSP11",
-    "D4":"dox 48 actin",
-    "D5":"dox 48 actin",
-    "D6":"dox 48 actin",
-    }
+    "A1": "mock 24 DUSP11",
+    "A2": "mock 24 DUSP11",
+    "A3": "mock 24 DUSP11",
+    "A4": "mock 24 actin",
+    "A5": "mock 24 actin",
+    "A6": "mock 24 actin",
+    "B1": "mock 48 DUSP11",
+    "B2": "mock 48 DUSP11",
+    "B3": "mock 48 DUSP11",
+    "B4": "mock 48 actin",
+    "B5": "mock 48 actin",
+    "B6": "mock 48 actin",
+    "C1": "dox 24 DUSP11",
+    "C2": "dox 24 DUSP11",
+    "C3": "dox 24 DUSP11",
+    "C4": "dox 24 actin",
+    "C5": "dox 24 actin",
+    "C6": "dox 24 actin",
+    "D1": "dox 48 DUSP11",
+    "D2": "dox 48 DUSP11",
+    "D3": "dox 48 DUSP11",
+    "D4": "dox 48 actin",
+    "D5": "dox 48 actin",
+    "D6": "dox 48 actin",
+}
+
+
 # Rename columns
 def rename_columns(df, rename):
     """Rename columns."""
@@ -124,7 +129,9 @@ def rename_columns(df, rename):
     df = df.dropna()
     return df
 
+
 melt_curve = rename_columns(melt_curve, rename)
+
 
 # Plot melt curve
 def plot_melt_curve(df, title):
@@ -135,15 +142,20 @@ def plot_melt_curve(df, title):
     # Combine mock and dox plots into one figure of separate plots
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 2, 1)
-    g_mock = sns.lineplot(data=df_mock, x="Reading", y="Derivative", hue="Sample Name")
+    g_mock = sns.lineplot(
+        data=df_mock, x="Temperature", y="Derivative", hue="Sample Name"
+    )
     g_mock.set_title(f"Mock - {title}")
-    g_mock.set(xlabel="Reading", ylabel="Derivative")
+    g_mock.set(xlabel="Temperature", ylabel="Derivative")
     plt.subplot(1, 2, 2)
-    g_dox = sns.lineplot(data=df_dox, x="Reading", y="Derivative", hue="Sample Name")
+    g_dox = sns.lineplot(
+        data=df_dox, x="Temperature", y="Derivative", hue="Sample Name"
+    )
     g_dox.set_title(f"Dox Treated - {title}")
-    g_dox.set(xlabel="Reading", ylabel="Derivative")
+    g_dox.set(xlabel="Temperature", ylabel="Derivative")
 
     plt.tight_layout()
     plt.savefig(f"TN031.9 Combined - {title}.png", dpi=300)
+
 
 plot_melt_curve(melt_curve, "Melt Curve")
