e0b405c80d2f51033efd07c49b7bd0d91281d22b
Thomas Nipper <nipper@wisc.edu>
2024-04-07 20:26:58 -0500

    Add t-test calculation and annotation for TN042.2.png

--- TN042/TN042.2.py
+++ TN042/TN042.2.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pingouin as pg
 
 data = pd.read_excel("TN042.2.xlsx")
 
@@ -23,5 +24,18 @@ g = sns.barplot(
 plt.yscale("log")
 plt.ylim(1e5, 1e7)
 plt.title("WSN Titer during dox induction")
+
+# Calculate the p-value
+ttest = pg.ttest(
+    data["Titer"][data["Virus"] == "CRISPRi - Dox"],
+    data["Titer"][data["Virus"] == "CRISPRi + Dox"],
+    paired=False,
+    alternative="two-sided",
+)
+ttest.to_excel("TN042.2_ttest.xlsx")
+def annotate_stats(pvalue, y, h):
+    plt.plot([0, 0, 1, 1], [y, y + h, y + h, y], lw=1, c="k")
+    plt.text(0.5, y + 2 * h, f"p = {pvalue:.2}", ha="center", va="center")
+annotate_stats(ttest["p-val"].values[0], 5e6, 5e5)
 
 plt.savefig("TN042.2.png", dpi=300)
