0c9f2b403461f6333c132f59fec4e8d10885712a
Thomas <nipper@wisc.edu>
2025-09-17 20:03:06 -0500

    update graph_levels.py for improved data processing and visualization; add new .DS_Store files

--- OMS/NP_levels_with_IFIT3/graph_levels.py
+++ OMS/NP_levels_with_IFIT3/graph_levels.py
@@ -3,30 +3,30 @@ import seaborn as sns
 import pandas as pd
 
 data = pd.read_excel('NP_IFIT3_levels_ddCT.xlsx')
-data_filtered = data[data['Target Name'] == 'WSN_NP']
-
+data_filtered = data[data['Target Name'] == 'WSN_NP'].reset_index(drop=True)
+data_filtered[['Sample', 'rep']] = data_filtered['Sample Name'].str.rsplit('_', n=1, expand=True)
 def plot_data(data, outname):
     # Assuming 'foldchange_se' contains the standard error values
     lower_error = data['foldchange_se']
     upper_error = data['foldchange_se']
     
-    ax = sns.barplot(
-        x='Sample Name', 
-        y='foldchange_mean', 
+    ax = sns.stripplot(
+        x='Sample', 
+        y='ddct_mean', 
         data=data
     )
-    ax.errorbar(
-        x=data['Sample Name'], 
-        y=data['foldchange_mean'], 
-        yerr=[lower_error, upper_error], 
-        fmt='none', 
-        capsize=5, 
-        color='black'
-    )
+    # ax.errorbar(
+    #     x=data['Sample Name'], 
+    #     y=data['ddct_mean'], 
+    #     yerr=[lower_error, upper_error], 
+    #     fmt='none', 
+    #     capsize=5, 
+    #     color='black'
+    # )
     ax.set_title('NP levels +- IFIT3')
-    ax.set_ylabel('Fold Change')
+    ax.set_ylabel('ddCT')
     ax.set_xlabel('')
-    custom_labels = ['NP only 1', 'NP only 2', 'NP only 3', 'NP + IFIT3 1', 'NP + IFIT3 2', 'NP + IFIT3 3']
+    custom_labels = ['NP only', 'NP + IFIT3']
     ax.set_xticklabels(custom_labels, rotation=45)
     plt.xticks(rotation=45)
     plt.tight_layout()
