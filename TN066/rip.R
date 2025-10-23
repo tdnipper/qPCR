38de19d32c668a70b2a9e07f83eb65067b69f754
Thomas <nipper@wisc.edu>
2025-10-23 17:53:18 -0500

    Refactor enrichment calculations and update plots; replace percent recovery with enrichment metrics and adjust plot titles and file names for clarity

--- TN066/rip.R
+++ TN066/rip.R
@@ -23,14 +23,14 @@ data_dct <- data %>%
                 .names = "delta_ct_mock")) %>%
   select(`Target Name`, starts_with("delta_ct_")) %>%
   mutate(across(.cols = starts_with("delta_ct_"),
-                .fns = ~ 2^-.x * 100,
-                .names = "percent_recovery_{.col}")) %>%
-  rename_with(~ sub("delta_ct_", "", .x), starts_with("percent_recovery_")) %>%
-  select(`Target Name`, starts_with("percent_recovery_")) %>%
+                .fns = ~ 2^-.x,
+                .names = "enrichment_{.col}")) %>%
+  rename_with(~ sub("delta_ct_", "", .x), starts_with("enrichment_")) %>%
+  select(`Target Name`, starts_with("enrichment_")) %>%
   pivot_longer(cols = -`Target Name`,
                names_to = "Condition",
-               values_to = "percent_recovery",
-               names_prefix = "percent_recovery_")
+               values_to = "enrichment",
+               names_prefix = "enrichment_")
 
 write_xlsx(data_dct, "TN066/rip_results.xlsx")
 
@@ -47,11 +47,11 @@ data_alt <- data_alt %>%
 write_xlsx(data_alt, "TN066/rip_dct_normalized.xlsx")
 
 # Plot percent recovery data
-p <- ggplot(data_dct, aes(x = `Target Name`, y = percent_recovery, color = Condition)) +
+p <- ggplot(data_dct, aes(x = `Target Name`, y = enrichment, color = Condition)) +
   geom_point(size = 2, alpha = 0.7) +
-  labs(title = "NP Recovery Infected vs Mock",
+  labs(title = "NP Enrichment Infected vs Mock",
     x = "Target Gene",
-    y = "Percent Recovery",
+    y = "% Input",
     color = "Status") +
   theme_classic() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
@@ -59,21 +59,9 @@ p <- ggplot(data_dct, aes(x = `Target Name`, y = percent_recovery, color = Condi
         axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
         axis.text.y = element_text(size = 12))
-ggsave("TN066/percent_recovery_plot.png", plot = p, width = 10, height = 6, dpi = 300)
+ggsave("TN066/percent_input.png", plot = p, width = 10, height = 6, dpi = 300)
 
-# Plot dct normalized data
-p2 <- ggplot(data_alt, aes(x = `Target Name`, y = dct_norm)) +
-  geom_point(size = 2, alpha = 0.7, color = "#253755") +
-  labs(title = "NP Enrichment DCT Infected - Mock",
-    x = "Target Gene",
-    y = "DCT - 18S") +
-  theme_classic() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
-        plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
-        axis.title.x = element_text(size = 14),
-        axis.title.y = element_text(size = 14),
-        axis.text.y = element_text(size = 12))
-ggsave("TN066/dct_normalized_plot.png", plot = p2, width = 10, height = 6, dpi = 300)
+# Plot dct data
 
 p3 <- ggplot(data_alt, aes(x = `Target Name`, y = dct)) +
   geom_point(size = 2, alpha = 0.7, color = "#253755") +
