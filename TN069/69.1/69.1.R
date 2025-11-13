15d987d17d63f631f664a33fbbe46a75b2a8424d
Thomas <nipper@wisc.edu>
2025-11-13 10:48:18 -0600

    separate data to overlay mock and infected on each timepoint

--- TN069/69.1/69.1.R
+++ TN069/69.1/69.1.R
@@ -6,24 +6,25 @@ df <- read.csv("TN069/69.1/TN069.1_filtered.csv")
 df <- df %>%
   group_by(Sample.Name, Target.Name) %>%
   summarize(CT = mean(CT, na.rm = FALSE), .groups = "drop") %>%
-  separate(Sample.Name, into = c("base_name", "type"), sep = "_(?=[^_]+$)") %>%
+  separate(Sample.Name, into = c("base_name", "timepoint", "type"), sep = "_") %>%
   pivot_wider(names_from = type, values_from = CT) %>%
-  mutate(percent_input = 100 * 0.2 * 2^(input - enrich)) %>% 
+  # calculate fold change using input dilution factor
   mutate(fc = 0.2 * 2^(input - enrich)) %>%
-  mutate(log2fc = log2(fc))
+  mutate(log2fc = log2(fc)) %>%
+  mutate(percent_input = fc * 100)
 write.csv(df, "TN069/69.1/69.1_results.csv", row.names = FALSE)
 
 # filter for FLUC only
 fluc <- df %>%
   filter(Target.Name == "FFLUC") %>%
-  select(base_name, percent_input)
+  select(base_name, timepoint, percent_input)
 
 # Plot FLUC percent input
-p <- ggplot(fluc, aes(x = base_name, y = percent_input)) +
+p <- ggplot(fluc, aes(x = timepoint, y = percent_input, color = base_name)) +
   geom_point() +
   theme_classic() +
   labs(title = "FLUC Recovery",
-       x = "sample_h.p.i.",
+       x = "Incubation Duration (hours)",
        y = "Percent Input"
   )
 print(p)
