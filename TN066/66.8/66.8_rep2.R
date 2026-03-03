06bad649ac4cf7fa3960bef1d456e6b2af913bcb
Thomas <nipper@wisc.edu>
2026-03-03 15:40:33 -0600

    Add data processing and visualization for TN066.8_rep2 analysis

---
+++ TN066/66.8/66.8_rep2.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+
+df <- read.csv("TN066/66.8/TN066.8_rep2_filtered.csv")
+
+data <- df %>%
+	group_by(Sample.Name, Target.Name, Task) %>%
+	summarise(CT = mean(CT, na.rm = TRUE), .groups = 'drop') %>%
+	pivot_wider(names_from = Sample.Name, values_from = CT)	
+
+data_enrich <- data %>%
+	mutate(mock_FLAG_norm = mock_FLAG - mock_IgG,
+		infect_FLAG_norm = infect_FLAG - infect_IgG,
+		mock_m2_norm = mock_M2 - mock_IgG,
+		infect_m2_norm = infect_M2 - infect_IgG) %>%
+	mutate(across(.cols = c(mock_FLAG_norm, infect_FLAG_norm, mock_m2_norm, infect_m2_norm),
+		~ 2^-.x))
+
+data_enrich_long <- data_enrich %>%
+    select(Target.Name, Task, mock_FLAG_norm, infect_FLAG_norm, mock_m2_norm, infect_m2_norm) %>%
+    pivot_longer(cols = c(infect_m2_norm, mock_m2_norm, infect_FLAG_norm, mock_FLAG_norm),
+        names_to = "Condition",
+        values_to = "normalized_foldchange",
+        names_prefix = "foldchange_")
+
+p <- ggplot(data_enrich_long, aes(x = Target.Name, y = normalized_foldchange, color = Condition)) +
+	geom_point(size = 2, position = position_dodge(width = 0.5)) +
+	theme_classic() + 
+	ylim(0, 5)
+ggsave("TN066/66.8/66.8_rep2_foldchange.png", plot = p, width = 10, height = 6, dpi=300)
+
+
+# df_amp <- read.csv("TN066/66.8/TN066.8_rep2_amp.csv") %>%
+# 	select(-Well) %>%
+# 	drop_na() %>%
+# 	group_by(Sample.Name, Target.Name, Cycle, Task) %>%
+# 	summarize(
+# 		Rn_mean = mean(Rn, na.rm = TRUE),
+# 		Rn_sd = sd(Rn, na.rm = TRUE),
+# 		.groups = "drop"
+# 	)
+# p_amp <- ggplot(df_amp, aes(x = Cycle, y = Rn_mean, color = Task)) +
+# 	geom_line() +
+# 	geom_ribbon(aes(ymin = Rn_mean - Rn_sd, ymax = Rn_mean + Rn_sd, fill = Task), alpha = 0.2, color = NA, show.legend = FALSE) +
+# 	theme_classic() + 
+# 	facet_grid(Target.Name ~ Sample.Name)
+# ggsave("TN066/66.8/66.8_amp.png", plot = p_amp, width = 10, height = 6, dpi=300)
