182a564d7fd0f5b8b2810bc7f844824ad6fc0f20
Thomas <nipper@wisc.edu>
2026-03-31 10:27:08 -0500

    Add data processing and visualization for TN066.10 analysis

---
+++ TN066/66.10/66.10.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+
+data <- read.csv("TN066/66.10/TN066.10_filtered.csv") %>%
+  group_by(Sample.Name, Target.Name) %>%
+  summarize(mean_ct = mean(CT, na.rm = TRUE), .groups = 'drop')
+
+data_eluate <- data %>%
+  filter(!grepl("input", Sample.Name, ignore.case = TRUE)) %>%
+  separate(Sample.Name, into = c("condition", "infection", "ab"), sep = "_") %>%
+  pivot_wider(names_from = ab, values_from = mean_ct) %>%
+  mutate(enrich = FAB - V5) %>%
+  mutate(enrich_foldchange = 2^-enrich)
+
+p <- ggplot(data_eluate, aes(x = Target.Name, y = enrich_foldchange, color = infection)) +
+  geom_point(size = 2, position = position_dodge(width = 0.5)) +
+  theme_classic() +
+  labs(title = "RIG-I RIP", x = "", y = "Fold Change (FAB - V5)", color = "", caption = "66.10")
+ggsave("TN066/66.10/TN066_10_enrichment_foldchange.png", plot = p, width = 10, height = 6, dpi=300)
